3c4f97b739ac16ffd554e0e53cb9ca7a7fa498fd
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-06-22 17:52:31 -0300

    adicionando Testes

---
+++ src/com/lucas/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,14 @@
+package com.lucas.estruturadados.vetor.teste;
+
+import com.lucas.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+
+        i++;
+    }
+}
---
+++ src/com/lucas/estruturadados/vetor/teste/Aula04.java
@@ -0,0 +1,16 @@
+package com.lucas.estruturadados.vetor.teste;
+
+import com.lucas.estruturadados.vetor.Vetor;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+
+    }
+
+}
